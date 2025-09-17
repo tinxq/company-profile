5901cf26da1cb4ef964d2b9b3e6b23d6929b3030
tinxq <tafakurtarig@gmail.com>
2025-09-18 01:13:41 +0300

    12

--- src/services/TelecomServices.js
+++ src/services/TelecomServices.js
@@ -11,32 +11,33 @@ export default function TelecomServices() {
         <div className="telecom-section">
           <div className="telecom-section-header">Telecom Operator Services</div>
           <ul className="telecom-section-list">
-            <li>Wireless Access Networks</li>
-            <li><img src="/imgs/img1.jpg"/></li>
-            <li>Fixed Access Networks</li>
-            <li><img src="/imgs/img2.jpg"/></li>
-            <li>Core Networks</li>
-            <li><img src="/imgs/img3.jpg"/></li>
-            <li>Transport Networks</li>
-            <li><img src="/imgs/img4.jpg"/></li>
-            <li>In Building Solution</li>
-            <li><img src="/imgs/img6.jpg"/></li>
-            <li><img src="/imgs/img5.png"/></li>
-          </ul>
-        </div>
+  <li>Wireless Access Networks</li>
+  <li><img src="/imgs/img1.jpg" alt="Wireless Access Networks illustration"/></li>
+  <li>Fixed Access Networks</li>
+  <li><img src="/imgs/img2.jpg" alt="Fixed Access Networks illustration"/></li>
+  <li>Core Networks</li>
+  <li><img src="/imgs/img3.jpg" alt="Core Networks illustration"/></li>
+  <li>Transport Networks</li>
+  <li><img src="/imgs/img4.jpg" alt="Transport Networks illustration"/></li>
+  <li>In Building Solution</li>
+  <li><img src="/imgs/img6.jpg" alt="In Building Solution illustration"/></li>
+  <li><img src="/imgs/img5.png" alt="Additional telecom service illustration"/></li>
+</ul>
 
-        <div className="telecom-section">
-          <div className="telecom-section-header">Installations</div>
-          <ul className="telecom-section-list">
-            <li>Microwave link</li>
-            <li><img src="/imgs/img7.jpg"/></li>
-            <li>point to point</li>
-            <li><img src="/imgs/img8.jpg"/></li>
-          </ul>
-        </div>
+<div className="telecom-section">
+  <div className="telecom-section-header">Installations</div>
+  <ul className="telecom-section-list">
+    <li>Microwave link</li>
+    <li><img src="/imgs/img7.jpg" alt="Microwave link illustration"/></li>
+    <li>point to point</li>
+    <li><img src="/imgs/img8.jpg" alt="Point to point installation illustration"/></li>
+  </ul>
+</div>
 
         <div className="telecom-footer-text">FIX ALARM CONFIG</div>
       </div>
-    </div></>
+    </div>
+    </div>
+    </>
   );
 }
