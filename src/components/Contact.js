0bef4e876dff0f212b698179b7e942c8226866af
tinxq <tafakurtarig@gmail.com>
2025-10-05 23:33:33 +0300

    25

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,40 @@
 import React from 'react';
+import { FaPhone, FaEnvelope, FaMapMarkerAlt, FaWhatsapp } from "react-icons/fa";
 
 function Contact() {
   return (
     <section id="contact" className="section">
       <h2>Get In Touch</h2>
-      <p><strong>Phone:</strong> 0576716123</p>
-       <p><strong>Phone:</strong> 0541653306</p>
-       <p><strong>Phone:</strong> 0502275061</p>
-       <p><strong>Email:</strong> Info@daralkhebra.it.com</p>
-      <p><strong>Address:</strong> Buraydah Al Qassim, Saudi Arabia</p>
+
+      <p>
+        <FaPhone style={{ marginRight: "8px", color: "#333" }} />
+        <FaWhatsapp style={{ marginRight: "8px", color: "green" }} />
+        <strong>Phone:</strong> 0576716123
+      </p>
+
+      <p>
+        <FaPhone style={{ marginRight: "8px", color: "#333" }} />
+        <FaWhatsapp style={{ marginRight: "8px", color: "green" }} />
+        <strong>Phone:</strong> 0541653306
+      </p>
+
+      <p>
+        <FaPhone style={{ marginRight: "8px", color: "#333" }} />
+        <FaWhatsapp style={{ marginRight: "8px", color: "green" }} />
+        <strong>Phone:</strong> 0502275061
+      </p>
+
+      <p>
+        <FaEnvelope style={{ marginRight: "8px", color: "#0073e6" }} />
+        <strong>Email:</strong> Info@daralkhebra.it.com
+      </p>
+
+      <p>
+        <FaMapMarkerAlt style={{ marginRight: "8px", color: "red" }} />
+        <strong>Address:</strong> Buraydah Al Qassim, Saudi Arabia
+      </p>
     </section>
   );
 }
+
 export default Contact;
